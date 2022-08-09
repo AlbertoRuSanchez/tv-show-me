015c83877ae1319390a48502346353ddd5e1f884
ARS <albertorusanchez@gmail.com>
2022-08-09 17:30:40 +0000

    Second commit

---
+++ src/main/java/com/craft/tvshowme/TvShowmeApplication.java
@@ -0,0 +1,13 @@
+package com.craft.tvshowme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvShowmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvShowmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/craft/tvshowme/TvShowmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craft.tvshowme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvShowmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
